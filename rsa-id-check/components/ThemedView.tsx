b9e7a01724a3599709c25910198e255211279c17
Willem Wannenburg <daeben72@gmail.com>
2024-05-26 12:54:58 +0200

    Modified color hooks to dark palette

--- rsa-id-check/components/ThemedView.tsx
+++ rsa-id-check/components/ThemedView.tsx
@@ -1,14 +1,17 @@
-import { View, type ViewProps } from 'react-native';
+import { ColorValue, View, type ViewProps } from "react-native";
 
-import { useThemeColor } from '@/hooks/useThemeColor';
+import { useColor } from "@/hooks/useColor";
+import { Colors } from "@/constants/Colors";
 
 export type ThemedViewProps = ViewProps & {
+  color?: keyof typeof Colors.light & keyof typeof Colors.dark;
   lightColor?: string;
   darkColor?: string;
 };
 
-export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
-  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
+export function ThemedView({ color, style, ...otherProps }: ThemedViewProps) {
+  const backgroundColor: ColorValue = color ? useColor(color) : "transparent";
+  console.log(`Returning background color: ${backgroundColor.toString()}`);
 
   return <View style={[{ backgroundColor }, style]} {...otherProps} />;
 }
